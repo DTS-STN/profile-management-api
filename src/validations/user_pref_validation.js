3294711ce9766b1f90f86b8a9f5fc33ccaad3d85
mjkabir40 <mjkabir40@gmail.com>
2021-12-21 15:23:39 -0500

    few changes based on internal review

--- src/validations/user_pref_validation.js
+++ src/validations/user_pref_validation.js
@@ -6,26 +6,26 @@ const createUserPref = {
   }),
   body: Joi.object().keys({
     webLanguageCode: Joi.number().min(1).max(2).required().messages({
-      "number.base": "Web language should be a Number",
+      "number.base": "Web language should be selected",
       "number.empty": "Web language should not be empty",
       "number.min": `Web language should be greater than or equal to {#limit}`,
       "number.max": `Web language should be less than or equal to {#limit}`,
       "any.required": "Web language is required",
     }),
     correspondenceLanguageCode: Joi.number().min(1).max(2).required().messages({
-      "number.base": "Correspondence language should be a Number",
-      "number.empty": "Correspondence language should not be empty",
-      "number.min": `Correspondence language should be greater than or equal to {#limit}`,
-      "number.max": `Correspondence language should be less than or equal to {#limit}`,
-      "any.required": "Correspondence language is required",
+      "number.base": "Communication language should be selected",
+      "number.empty": "Communication language should not be empty",
+      "number.min": `Communication language should be greater than or equal to {#limit}`,
+      "number.max": `Communication language should be less than or equal to {#limit}`,
+      "any.required": "Communication language is required",
     }),
     brailleTtyKeyboard: Joi.boolean().required().messages({
-      "boolean.base": "Braille/TTY should be true or false",
+      "boolean.base": "Braille/TTY should be selected",
       "boolean.empty": "Braille/TTY should not be empty",
       "any.required": "Braille/TTY is required",
     }),
     preferredCurrencyCode: Joi.number().min(1).max(2).required().messages({
-      "number.base": "Preferred Currency Code should be a Number",
+      "number.base": "Preferred Currency Code should be selected",
       "number.empty": "Preferred Currency Code should not be empty",
       "number.min": `Preferred Currency Code should be greater than or equal to {#limit}`,
       "number.max": `Preferred Currency Code should be less than or equal to {#limit}`,
@@ -38,7 +38,7 @@ const createUserPref = {
       "any.required": "Time Zone is required",
     }),
     timeFormatCode: Joi.number().min(1).max(2).required().messages({
-      "number.base": "Time Format Code should be a Number",
+      "number.base": "Time Format Code should be selected",
       "number.empty": "Time Format Code should not be empty",
       "number.min": `Time Format Code should be greater than or equal to {#limit}`,
       "number.max": `Time Format Code should be less than or equal to {#limit}`,
@@ -60,7 +60,7 @@ const updateUserPref = {
   body: Joi.object()
     .keys({
       webLanguageCode: Joi.number().min(1).max(2).required().messages({
-        "number.base": "Web language should be a Number",
+        "number.base": "Web language should be selected",
         "number.empty": "Web language should not be empty",
         "number.min": `Web language should be greater than or equal to {#limit}`,
         "number.max": `Web language should be less than or equal to {#limit}`,
@@ -71,19 +71,19 @@ const updateUserPref = {
         .max(2)
         .required()
         .messages({
-          "number.base": "Correspondence language should be a Number",
-          "number.empty": "Correspondence language should not be empty",
-          "number.min": `Correspondence language should be greater than or equal to {#limit}`,
-          "number.max": `Correspondence language should be less than or equal to {#limit}`,
-          "any.required": "Correspondence language is required",
+          "number.base": "Communication language should be selected",
+          "number.empty": "Communication language should not be empty",
+          "number.min": `Communication language should be greater than or equal to {#limit}`,
+          "number.max": `Communication language should be less than or equal to {#limit}`,
+          "any.required": "Communication language is required",
         }),
       brailleTtyKeyboard: Joi.boolean().required().messages({
-        "boolean.base": "Braille/TTY should be true or false",
+        "boolean.base": "Braille/TTY should be selected",
         "boolean.empty": "Braille/TTY should not be empty",
         "any.required": "Braille/TTY is required",
       }),
       preferredCurrencyCode: Joi.number().min(1).max(2).required().messages({
-        "number.base": "Preferred Currency Code should be a Number",
+        "number.base": "Preferred Currency Code should be selected",
         "number.empty": "Preferred Currency Code should not be empty",
         "number.min": `Preferred Currency Code should be greater than or equal to {#limit}`,
         "number.max": `Preferred Currency Code should be less than or equal to {#limit}`,
@@ -96,7 +96,7 @@ const updateUserPref = {
         "any.required": "Time Zone is required",
       }),
       timeFormatCode: Joi.number().min(1).max(2).required().messages({
-        "number.base": "Time Format Code should be a Number",
+        "number.base": "Time Format Code should be selected",
         "number.empty": "Time Format Code should not be empty",
         "number.min": `Time Format Code should be greater than or equal to {#limit}`,
         "number.max": `Time Format Code should be less than or equal to {#limit}`,
